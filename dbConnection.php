9c9380a2b5c7ff4743c6b029bff9759bd848a7a0
Alessandro Fael Garcia <alessfg@hotmail.com>
2016-02-28 18:21:51 +0000

    Updated Connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,11 +1,10 @@
 <?php
-$DB_host = "localhost";
-$DB_user = "ebay_admin";
-$DB_pass = "192837465";
-$DB_name = "ebay_clone";
+$dsn = 'mysql:host=localhost;dbname=ebay_clone';
+$db_user = 'ebay_admin';
+$db_pass = '192837465';
 
 try{
-    $db = new PDO("mysql: host = {$DB_host}; dbname = {$DB_name}", $DB_user, $DB_pass);
+    $db = new PDO($dsn, $db_user, $db_pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e){
