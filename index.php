128dd73a52cb4c8c62347c286cfad24275fb6eee
laurenta2 <ucabaur@ucl.ac.uk>
2016-03-17 14:22:15 +0000

    Updated index
    Login changed

--- index.php
+++ index.php
@@ -70,23 +70,10 @@ require_once('dbConnection.php');
                                     <div class="form-group">
                                         <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
                                     </div>
-                                    <div class="form-group text-center">
-                                        <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-                                        <label for="remember"> Remember Me</label>
-                                    </div>
                                     <div class="form-group">
                                         <div class="row">
                                             <div class="col-sm-6 col-sm-offset-3">
                                                 <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-lg-12">
-                                                <div class="text-center">
-                                                    <a href="recover.php" tabindex="5" class="forgot-password">Forgot Password?</a>
-                                                </div>
                                             </div>
                                         </div>
                                     </div>
